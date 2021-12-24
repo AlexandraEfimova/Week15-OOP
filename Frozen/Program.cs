a74be8ac9b4b47350dc0e6194543d6659cafab51
Alexandra Efimova <aefimova@tktk.ee>
2021-12-24 16:58:01 +0200

    add new projects

---
+++ Frozen/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Frozen
+{
+    class Program
+    {
+        class Gifts
+        {
+            string name;
+            string thing;
+
+            public Gifts(string _name, string _thing)
+            {
+                name = _name;
+                thing = _thing;
+            }
+
+            public string Name
+            {
+                get { return name; }
+            }
+            public string Thing
+            {
+                get { return thing; }
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            List<Gifts> myGifts = new List<Gifts>();
+            string[] giftsFromFile = GetDataFromFile();
+
+            foreach (string line in giftsFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+                Gifts newGift = new Gifts(tempArray[0], tempArray[1]);
+                myGifts.Add(newGift);
+
+            }
+
+            foreach (Gifts giftFromList in myGifts)
+            {
+                Console.WriteLine($" {giftFromList.Name} wants {giftFromList.Thing} for Christmas.");
+            }
+        }
+
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach (string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\Alexandra\Documents\frozen\frozen.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+}
---
+++ OOPAndFiles/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OOPAndFiles
+{
+    class Program
+    {
+        class Movies
+        {
+            string title;
+            string rating;
+            string year;
+
+            public Movies(string _title, string _rating, string _year)
+            {
+                title = _title;
+                rating = _rating;
+                year = _year;
+            }
+
+            //getters for movie
+
+            public string Title
+            {
+                get { return title; }
+            }
+            public string Rating
+            {
+                get { return rating; }
+            }
+            public string Year
+            {
+                get { return year; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            List<Movies> myMovies = new List<Movies>();
+            string[] moviesFromFile = GetDataFromFile();
+
+            foreach(string line in moviesFromFile)
+            {
+                string[] tempArray = line.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                Movies newMovie = new Movies(tempArray[0], tempArray[1], tempArray[2]);
+                myMovies.Add(newMovie);
+
+            }
+
+            foreach(Movies movieFromList in myMovies)
+            {
+                Console.WriteLine($"Title --> {movieFromList.Title}. Rating --> {movieFromList.Rating}. Year --> {movieFromList.Year}.");
+            }
+        }
+        public static void DisplayElementsFromArray(string[] someArray)
+        {
+            foreach(string element in someArray)
+            {
+                Console.WriteLine($"String from array: {element}");
+            }
+        }
+        public static string[] GetDataFromFile()
+        {
+            string filePath = @"C:\Users\Alexandra\Documents\movies\movies.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+
+            return dataFromFile;
+        }
+    }
+    
+
+
+
+}
